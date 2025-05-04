a8521243e530773d088370830aaf5bf9e5b2374c
KevinRawal <kevinrawal30@gmail.com>
2025-05-04 09:59:43 +0530

    added navbar

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import { Route, Routes } from 'react-router'
 import Home from './components/Home'
+import About from './components/About'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
+    <>
+    <Navbar/>
       <Routes>
         {/* index is used to make home default entry point */}
         <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
+    </>
   )
 }
 
